82915d9763cdcaea4b4812af171fda89fbb93cf3
David Parsons <david.parsons@rutgers.edu>
2018-04-16 19:27:25 -0400

    Add Arguement Parsing

--- client.py
+++ client.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
-import argparse
+import argparse, json
+import requests
+
+def login(authentication_dict):
+    # Takes in body of json dictionary used to authenticate
+    # Sends GET request to the "/login" resource
+    # Returns
+    # {
+    #     "authenticated" : True or False,
+    #     "Response" : "<Response Message>"
+    # }
+    pass
 
 def main():
-    pass
+    # parse all the arguments to the client
+    parser = argparse.ArgumentParser(description='CS 419 OpenSSL Message Board Client')
+    parser.add_argument('-d','--destination', help='Destination IP Host', required=False, default='127.0.0.1')
+    parser.add_argument('-p','--remoteport', help='remote port', required=False, default='8080')
+
+    args = vars(parser.parse_args())
+    destinationIP = args['destination']
+    remote_port = args['remoteport']
+
+    # Authenticate
+    requests.get("http://%s:%s/" % (destinationIP, remote_port))
+    # Now Accept CLI commands until death
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt as e:
+        print("Goodbye!")
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
-import argparse
+import argparse, json
+from http.server import HTTPServer
+
+def test():
+    print("Handled")
 
 def main():
-    pass
+    # parse all the arguments to the server
+    parser = argparse.ArgumentParser(description='CS 419 OpenSSL Message Board Server')
+    parser.add_argument('-l','--localport', help='local port', required=False, default="8080")
+    args = vars(parser.parse_args())
+    local_port =  int(args['localport'])
+
+    print('Starting Server...')
+    server_address = ('', local_port)
+    httpd = HTTPServer(server_address, test)
+    print('Running Server on port %d...' % (local_port))
+    httpd.serve_forever()
 
 if __name__ == '__main__':
     main()
